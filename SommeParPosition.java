b89622cf487bd5364bbf4838459ebfdb547ef5ae
Sunchock <toto.marestin@gmail.com>
2021-01-04 04:47:18 +0100

    Update SommeParPosition

--- SommeParPosition.java
+++ SommeParPosition.java
@@ -1,4 +1,4 @@
-//import java.util.*;
+import java.util.*;
 
 /**
   * SommeParPosition 
@@ -7,7 +7,41 @@
   */
 
 public class SommeParPosition extends Traitement {
+  private class TotalParPosition {
+    public Position position;
+    public double valeur;
 
-	// TODO à faire...
+    public TotalParPosition(Position position, double valeur) {
+      this.position = position;
+      this.valeur = valeur;
+    }
+  }
 
+  private List<TotalParPosition> positions = new ArrayList<TotalParPosition>();
+
+  @Override
+  public final void gererFinLotLocal(String nomLot) {
+    System.out.println("SommeParPosition " + nomLot);
+    for (TotalParPosition s : this.positions) {
+      System.out.println(" - " + s.position + "\t-> " + s.valeur);
+    }
+    System.out.println("Fin SommeParPosition.");
+	}
+
+  @Override
+  public final void traiter(Position position, double valeur) {
+    boolean find = false;
+
+    for (TotalParPosition s : this.positions) {
+      if (s.position.equals(position)) {
+        find = true;
+        s.valeur += valeur;
+        break;
+      }
+    }
+    if (!find) {
+      Collections.addAll(this.positions, new TotalParPosition(position, valeur));
+    }
+    super.traiter(position, valeur);
+  }
 }
